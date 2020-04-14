a350f9bd69cb7ef7733bc3d099d54beb306e7bc4
Jan Kibort <jan.kibort@eisolutions.eu>
2020-04-14 22:24:04 +0200

    zadanie1- wyliczanie średniej arytmetycznej

--- app/src/main/java/pl/jaz/jazapp/AverageServlet.java
+++ app/src/main/java/pl/jaz/jazapp/AverageServlet.java
@@ -5,8 +5,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.math.*;
+import java.text.DecimalFormat;
 
-@WebServlet("hello")
+@WebServlet("average")
 public class AverageServlet extends HttpServlet {
 
     @Override
@@ -14,7 +16,33 @@ public class AverageServlet extends HttpServlet {
         resp.setStatus(200);
         resp.setContentType("text/plain");
 
+        var numbers = req.getParameter("numbers");
         var writer = resp.getWriter();
-        writer.println("Hello World");
+
+        if (numbers == null || numbers.isEmpty()) {
+            writer.println("Please put parameters");
+        } else {
+            String[] myNumbers = numbers.split(",");
+
+            var counter = 0;
+            Float value = 0f;
+
+            for (int i = 0; i < myNumbers.length; i++) {
+                if (Character.isDigit(myNumbers[i].charAt(0))) {
+                    value += Float.parseFloat(myNumbers[i]);
+                    counter++;
+                } else {
+                    writer.println("Wrong parameter given");
+                    return;
+                }
+            }
+
+            var average = value / counter;
+
+            DecimalFormat n = new DecimalFormat("0.##");
+            n.setRoundingMode(RoundingMode.HALF_UP);
+
+            writer.println(n.format(average));
+        }
     }
 }
